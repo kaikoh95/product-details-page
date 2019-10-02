3f11a618400bb9533dca4cbdf6e27277cd479bfc
kaikoh95 <kaikoh95@gmail.com>
2019-10-03 03:01:48 +1300

    Develop: custom NavBar and cartReducer >>
    - added src/components/NavBar.js
    - added src/components/reducers/cartReducer.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {connect} from "react-redux";
+import MiniCart from "./MiniCart";
+import {Dropdown} from "semantic-ui-react";
+
+
+class Navbar extends Component{
+    render() {
+        return (
+            <nav className="nav-wrapper">
+                <ul className="cart-right">
+                    <Dropdown icon={null} style={{color: '#888888'}} text={"My Cart (" + this.props.subTotal + ")"}>
+                        <Dropdown.Menu direction={"left"} className="dropdown-menu">
+                            <MiniCart/>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+const mapStateToProps = (state)=>{
+    return {
+        subTotal: state.subTotal
+    }
+};
+
+export default connect(mapStateToProps)(Navbar);
